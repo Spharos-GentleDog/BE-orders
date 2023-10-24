bc6fb935257f4367967fdd1dc7954f23bcf9681a
Hwan0518 <qip0518@naver.com>
2023-10-24 11:14:21 +0900

    feat: 카드결제 api 구현완료
    - payment : card,cancels,easypay,payment,virtualAccount 생성 및 Payment객체에서 해당하는 정보만 받아서 저장
    - webclient : webclient 기본설정 및 외부서버 api request에서 사용

--- src/main/java/egenius/orders/domain/payment/entity/Payment.java
+++ src/main/java/egenius/orders/domain/payment/entity/Payment.java
@@ -1,16 +1,13 @@
 package egenius.orders.domain.payment.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
+import lombok.*;
 
+import java.util.List;
+
+@Getter
 @Entity
-@Builder
+@Builder(toBuilder = true)
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Payment {
@@ -18,4 +15,61 @@ public class Payment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "payment_key")
+    private String paymentKey;
+
+    @Column(name = "type")
+    private String type;
+
+    @Column(name = "order_id")
+    private String orderId;
+
+    @Column(name = "order_name")
+    private String orderName;
+
+    @Column(name = "m_id")
+    private String mId;
+
+    @Column(name = "method")
+    private String method;
+
+    @Column(name = "total_amount")
+    private Integer totalAmount;
+
+    @Column(name = "balance_amount")
+    private Integer balanceAmount;
+
+    @Column(name = "status")
+    private String status;
+
+    @Column(name = "requested_at") //todo: datetime으로 바꾸기
+    private String requestedAt;
+
+    @Column(name = "approved_at")
+    private String approvedAt;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "cancels_id", referencedColumnName = "id")
+    private Cancels cancels;
+
+    @Column(name = "isPartialCancelable")
+    private Boolean isPartialCancelable;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "card", referencedColumnName = "id")
+    private Card card;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "virtual_account", referencedColumnName = "id")
+    private VirtualAccount virtualAccount;
+
+    @Column(name = "receipt_url")
+    private String receipt_url; // receipt의 url과 매핑됨
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "easy_pay", referencedColumnName = "id")
+    private EasyPay easyPay;
+
+
 }
