f0738bb761e5c22115fe97a949b66269438725cc
Hwan0518 <qip0518@naver.com>
2023-11-08 18:04:00 +0900

    feat: Payment History 정산서버로 전송
    - Spring Batch + Kafka 사용해서 하루치 결제내역을 Settlement서버로 전송

--- src/main/java/egenius/orders/domain/batch/presentation/BatchController.java
+++ src/main/java/egenius/orders/domain/batch/presentation/BatchController.java
@@ -32,14 +32,18 @@ public class BatchController {
      */
 
     //1. 결제 내역, 정산 서버로 전송
+    //todo: 스케쥴러로 작동하게 만들기
     @GetMapping("")
     public BaseResponse paymentDataTransferBatch() {
         Date today = Date.from(LocalDate.now().atStartOfDay(ZoneId.systemDefault()).toInstant());
+        // jobParams : job을 실행할때 넘겨주고싶은 파라미터 & job을 고유하게 식별하는 역할
+        // 중복방지 : jobParams에 addDate만 남겨두면, 키는 일정하고 params도 하루동안은 일정하기에 중복 실행을 방지할 수 있음
         JobParameters jobParameters = new JobParametersBuilder()
-                        .addDate("Date:"+LocalDateTime.now(), today)
-                        .toJobParameters();
+                .addDate("Date", today)
+                .addLong("time",System.currentTimeMillis())
+                .toJobParameters();
         try {
-            JobExecution jobExecution = jobLauncher.run(paymentJob, jobParameters);
+            JobExecution jobExecution = jobLauncher.run(paymentJob,jobParameters);
             if (jobExecution.getExitStatus().equals(ExitStatus.COMPLETED)) {
                 log.info("job success");
             } else {
